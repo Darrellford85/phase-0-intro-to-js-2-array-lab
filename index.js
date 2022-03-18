39566ee43f4483cda608c102582ab4d5eda36133
Darrell Ford <Darrellford85@gmail.com>
2022-03-17 20:22:42 -0700

    commit tree 88f490fa15db0c2b5a16778d47486e0a3c51875c

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
-// Write your solution here!
+const cats =['Milo','Otis','Garfield'];
+
+function  destructivelyAppendCat(name) {
+    cats.push('Ralph');
+    return cats
+}
+function appendcat(name){
+    let newArray =[]
+    let cats= cats.const(newArray)
+    cats.push(Ralph)
+    return cats
+}
+function destructivelyPrependCat(name){
+    cats.unshift('Bob');
+    return cats
+}
+function PrependCat(name){
+    let newArray =[]
+    let cats = cats.const(newArray)
+    cats.unshift(Bob);
+    return cats
+}
+function destructivelyRemoveLastCat(name){
+    cats.pop('Garfield');
+    return cats
+}
+function removeLastCat(name){
+    let newArray =[]
+    let cats = cats.const(newArray)
+    cats.pop(Garfield);
+    return cats
+}
+function destructivelyRemoveFirstCat(name){
+    cats.shift('Milo');
+    return cats
+}
+function removeFirstCat(name){
+    let newArray =[]
+    let cats = cats.const(newArray)
+    cats.shift(Milo);
+    return cats
+}
